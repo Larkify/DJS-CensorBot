3f02c7c1d4f6f4321aaf8fb866d65b61440fce33
Kiran <69719309+Larkify@users.noreply.github.com>
2021-03-14 21:34:06 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
     // -- Main Config -- //
     prefix: "!", // How to call your commands
-    token: 'Nzg5OTI5Nzc0Njg3NDUzMTg0.X95Nbg.m0q40M28iUm4fXvlZ94PCKmGcf8', // The token for your Discord bot, found at https://discord.com/developers/
+    token: '', // The token for your Discord bot, found at https://discord.com/developers/
     banned_words: ["test30", "n word", "f slur"], // Words for Tony to filter out
-    log_channel: '816327956661665792', // The channel ID to send logs
+    log_channel: '123456789101112134', // The channel ID to send logs
+    //        
     // -- Status Stuff -- //
     status_text: `!help | larkx.xyz`, // Status text of Bot
     status_activity: `idle`, // Choose from "idle/online/dnd"
